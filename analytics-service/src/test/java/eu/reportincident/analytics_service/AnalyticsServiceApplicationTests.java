b66bcd1e8ba87ce744d53f66cf41f0116d267633
Vanja Đenadija <130909026+vanja-djenadija@users.noreply.github.com>
2025-04-22 18:39:31 +0200

    Enabling all microservices to use Cloud Config Server

---
+++ analytics-service/src/test/java/eu/reportincident/analytics_service/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.reportincident.analytics_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
