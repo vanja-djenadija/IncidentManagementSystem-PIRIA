33d4e7e990afb2df4c6c94405a07a181c4e250b7
Vanja Đenadija <130909026+vanja-djenadija@users.noreply.github.com>
2025-04-25 07:49:04 +0200

    Gateway gets routes from config-server

--- gateway-service/src/main/java/eu/reportincident/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/eu/reportincident/gateway_service/GatewayServiceApplication.java
@@ -11,18 +11,6 @@ import org.springframework.context.annotation.Bean;
 @EnableDiscoveryClient
 public class GatewayServiceApplication {
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("incident-service", r -> r.path("/incident-service/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri("lb://INCIDENT-SERVICE"))
-                .route("moderation-service", r -> r.path("/moderation-service/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri("lb://MODERATION-SERVICE"))
-                .build();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
